aaaa7d731b9437f13aeaeb61ac9745527e33869a
Angelitooooooo <otilegna00@gmail.com>
2026-02-20 15:19:50 +0800

    adding printing of qr code with specific printer

--- server/seeds/002_initial_kanbans.js
+++ server/seeds/002_initial_kanbans.js
@@ -4,54 +4,60 @@ exports.seed = async function(knex) {
   await knex('kanbans').del();
 
   // Inserts seed entries
-  await knex('kanbans').insert([
+  const baseKanbans = [
     {
-      id: 1,
       name: 'Project Alpha',
       data_set: '5',
       station: '1'
     },
     {
-      id: 2,
       name: 'Project Beta',
       data_set: '6',
       station: '2'
     },
     {
-      id: 3,
       name: 'Project Gamma',
       data_set: '7',
       station: '1'
     },
     {
-      id: 4,
       name: 'Project Delta',
       data_set: '5',
       station: '2'
     },
     {
-      id: 5,
       name: 'Project Epsilon',
       data_set: '6',
       station: '1'
     },
     {
-      id: 6,
       name: 'Project Zeta',
       data_set: '4',
       station: '2'
     },
     {
-      id: 7,
       name: 'Project Eta',
       data_set: '5',
       station: '1'
     },
     {
-      id: 8,
       name: 'Project Theta',
       data_set: '7',
       station: '2'
     }
-  ]);
+  ];
+
+  const extraCount = 40;
+  const extraKanbans = [];
+  for (let i = 1; i <= extraCount; i += 1) {
+    const station = String((i % 2) + 1);
+    const dataSet = String(4 + (i % 5));
+    extraKanbans.push({
+      name: `Batch ${String(i).padStart(3, '0')}`,
+      data_set: dataSet,
+      station
+    });
+  }
+
+  await knex('kanbans').insert([...baseKanbans, ...extraKanbans]);
 };
